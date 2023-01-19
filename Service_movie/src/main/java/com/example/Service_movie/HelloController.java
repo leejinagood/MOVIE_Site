5d46c6980834620687456a1971e6a252707c5f8c
이진아 <mnb2098@naver.com>
2023-01-19 15:16:20 +0900

    포트번호 7090 변경

--- Service_movie/src/main/java/com/example/Service_movie/HelloController.java
+++ Service_movie/src/main/java/com/example/Service_movie/HelloController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
         public static void main(String[] args) {
             SpringApplication.run(com.example.Service_movie.ServiceMovieApplication.class, args);
         }
-
     @GetMapping("/")
     public String index() {
         return "index.html";
