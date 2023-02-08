39ba3ea68a50dcfc475cbebaf318e4ef18bbe7e5
nova_1221 <yongho1221@gmail.com>
2023-02-08 14:35:27 +0900

    ReservationController 클래스에 영화 예약 창 매핑 생성

--- Service_movie/src/main/java/com/example/MovieService/Reserve/ReservationController.java
+++ Service_movie/src/main/java/com/example/MovieService/Reserve/ReservationController.java
@@ -1,11 +1,18 @@
 package com.example.MovieService.Reserve;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/reserve")
 public class ReservationController {
 
-
+    @ResponseBody
+    @GetMapping("/{movieId}")
+    public String reserve(@PathVariable long movieId) {
+        return "영화 아이디 : {movieId} 예약 창";
+    }
 }
