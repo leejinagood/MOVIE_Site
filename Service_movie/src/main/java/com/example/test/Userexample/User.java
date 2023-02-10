8f9f8bd687f2a4ced0bd29278b91e5bd8a1d6adf
이진아 <mnb2098@naver.com>
2023-02-10 16:42:25 +0900

    로그인 구현 중..

---
+++ Service_movie/src/main/java/com/example/test/Userexample/User.java
@@ -0,0 +1,19 @@
+//package com.example.UserService;
+//
+//
+//import lombok.Data;
+//
+//
+//@Data
+//public class User {
+//    private String userid;
+//    private String email;
+//    private String password;
+//
+//    public User() {
+//    }
+//
+//    public User(String userid, String email, String password) {
+//
+//    }
+//}
