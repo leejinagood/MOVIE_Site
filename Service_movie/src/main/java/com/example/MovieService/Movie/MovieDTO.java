00777a078f628d299d13cf5680e779b95407233f
Gamemode1221 <yongho1221@gmail.com>
2023-03-02 11:07:15 +0900

    MovieDTO 클래스 Colum 어노테이션 추가 ReserveDTO 클래스 Colum 어노테이션 추가 필요없는 Seat 클래스 제거

--- Service_movie/src/main/java/com/example/MovieService/Movie/MovieDTO.java
+++ Service_movie/src/main/java/com/example/MovieService/Movie/MovieDTO.java
@@ -4,18 +4,32 @@ import com.example.MovieService.ItemDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.Date;
 
 @Getter @Setter @Entity
 public class MovieDTO extends ItemDTO {
 
+    @Column(name = "rating")
     private float rating;       // 영화 평점 ex) 0.0 ~ 5.0
+
+    @Column(name = "total")
     private Long total;         // 누적 관객수
+
+    @Column(name = "genre")
     private String genre;       // 영화 장르
+
+    @Column(name = "releaseDate")
     private Date releaseDate;   // 영화 개봉 일자
+
+    @Column(name = "director")
     private String director;    // 영화 감독
+
+    @Column(name = "actor")
     private String actor;       // 출연자(배우)
+
+    @Column(name = "info")
     private String info;        // 영화 정보(줄거리 등)
 
     public MovieDTO() {
--- Service_movie/src/main/java/com/example/MovieService/Reserve/ReserveDTO.java
+++ Service_movie/src/main/java/com/example/MovieService/Reserve/ReserveDTO.java
@@ -4,6 +4,7 @@ import com.example.MovieService.ItemDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.Date;
 import java.util.Vector;
@@ -11,27 +12,46 @@ import java.util.Vector;
 @Getter @Setter @Entity
 public class ReserveDTO extends ItemDTO {
 
+    @Column(name = "time")
     private Date time;              // 영화 예약 시간
+
+    @Column(name = "theater")
     private String theater;         // 영화 상영관 숫자 ex) 1관, 2관
+
+    @Column(name = "type")
     private String type;            // 영화 상영 타입 ex) 자막, 더빙
+
+    @Column(name = "price")
     private Long price;             // 영화 가격
+
+    @Column(name = "totalSeat")
     private Long totalSeat;         // 영화 상영관 총 좌석 수(상영관 크기)
+
+    @Column(name = "leftSeat")
     private Long leftSeat;          // 영화 상영관 남은 좌석 수
-    private Vector<Seat> seat;      // 영화 상영관 좌석 행열 ex) D03, F11, H16
+
+    @Column(name = "seatHorizontal")
+    private String seatHorizontal;  // 영화 상영관 좌석 열 ex) D, F, H
+
+    @Column(name = "seatVertical")
+    private String seatVertical;    // 영화 상영관 좌석 행 ex) 03, 11, 16
+
+    @Column(name = "headcount")
     private int headcount;          // 영화 예약 희망 인원 수
 
     public ReserveDTO() {
     }
 
-    public ReserveDTO(String itemName, Date time, String theater, String type, Long price, Long totalSeat, Long leftSeat, Vector<Seat> seat, int headcount) {
-        super(itemName);
+    public ReserveDTO(Date time, String theater, String type, Long price, Long totalSeat, Long leftSeat, String seatHorizontal, String seatVertical, int headcount) {
         this.time = time;
         this.theater = theater;
         this.type = type;
         this.price = price;
         this.totalSeat = totalSeat;
         this.leftSeat = leftSeat;
-        this.seat = seat;
+        this.seatHorizontal = seatHorizontal;
+        this.seatVertical = seatVertical;
         this.headcount = headcount;
     }
+
 }
